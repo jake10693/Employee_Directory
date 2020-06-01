0973dad52c1ea0a3348cc5640424881003c9acae
Jacob Carver <jake10693@gmail.com>
2020-06-01 13:24:51 -0700

    ran npm build

---
+++ build/precache-manifest.008c5bfa796e578c394183ccf5fd68cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "020863c58f3480ea3f5e63893afb87dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a90d15a22f876143773",
+    "url": "/static/css/main.a9ad2a8a.chunk.css"
+  },
+  {
+    "revision": "98d381b8539990c9da13",
+    "url": "/static/js/2.88f9d077.chunk.js"
+  },
+  {
+    "revision": "1a90d15a22f876143773",
+    "url": "/static/js/main.793ae791.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
